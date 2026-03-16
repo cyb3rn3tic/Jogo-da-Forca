1b34ad4390d16bfa2bca40a297ad12b041b8a0d8
Osmar Steffen <steffenosmar@gmail.com>
2026-03-16 19:57:08 -0300

    começo do prog

---
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace JogoDaForca.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Jogo da Forca:");
+            Console.WriteLine("---------------------------");
+
+            // 1. Inicio, deve ser selecionado uma palavra aleatoria de uma lista
+
+            Console.Write("Deseja continuar o jogo ? (S/N): ");
+            string? opcaoContinuar = Console.ReadLine()?.ToUpper();
+
+            JogoDaForca jogo = new JogoDaForca(palavra);
+            jogo.Jogar();
+        }
+    }
+
+    static string EscolherPalavraAleatoria()
+    {
+        Console.WriteLine
+    }
+
+}
+
